8b8bf472e9f8437f7fb91cd83d1d4a2d5af5dc2e
Kobe Dieryck <114076056+TheNeg0t1ator@users.noreply.github.com>
2023-12-09 23:26:00 +0100

    source rework done

---
+++ bracketingTour.cpp
@@ -0,0 +1,10 @@
+#include "bracketingTour.h"
+
+int BracketingTour::DoTour(){
+    //* Insert code here to create the list for bracketing rankings
+    // check for buy.
+    
+}
+int BracketingTour::InitializeTour(){
+    //* Insert code here to initialize bracketing rankings
+}
--- game.h
+++ game.h
@@ -22,21 +22,4 @@ private:
     int                     roundCounter;
 };
 
-void game::addPlayer(Player newplayer)
-{
-        PlayerWithPoints newclass;
-        newclass.inputPlayer(newplayer);
-        players.emplace_back(newclass);
-}
-
-void game::addPlayerName(string input)
-{
-        PlayerWithPoints newclass;
-        hash<string> hasher;
-        newclass.Changename(input);
-        newclass.Changeid(hasher(input));
-        players.emplace_back(newclass);
-}
-
-
 #endif

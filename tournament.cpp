469ed9d982f8524eecda3c9ec1f53a6bfd635b98
Kobe Dieryck <114076056+TheNeg0t1ator@users.noreply.github.com>
2023-12-10 16:27:18 +0100

    added comments

--- tournament.cpp
+++ tournament.cpp
@@ -16,12 +16,12 @@ void Tournament::SortGames(vector<game *>& input){
 }
 
 int  Tournament::ScoreCompare(PlayerWithPoints & inputA, PlayerWithPoints & inputB){
-    PointStruct pointA = inputA.GetPoints();
+    PointStruct pointA = inputA.GetPoints();//* getting the pointstructs
     PointStruct pointB = inputB.GetPoints();
-    int compare = PointCompare(pointA.totalPoints, pointB.totalPoints);
-    if(compare != 0){
+    int compare = PointCompare(pointA.totalPoints, pointB.totalPoints);//* comparing for normal points
+    if(compare != 0){//* if they aren't tied return
         return compare;
-    }else{
+    }else{//* if they are tied check for the total x;
         return PointCompare(pointA.total_x, pointB.total_x);
     } 
 
@@ -31,11 +31,11 @@ int  Tournament::ScoreCompare(PlayerWithPoints & inputA, PlayerWithPoints & inpu
 
 int Tournament::PointCompare(uint16_t a = 0, uint16_t b = 0){
     if (a > b){
-        return 1;
+        return 1;//*  return if a is higher
     }else if (a = b){
-        return 0;
+        return 0;//*  return if a is equal
     }else if (a < b){
-        return -1;
+        return -1;//* return if a is lower
     }
     return 0;
 }
@@ -43,15 +43,15 @@ int Tournament::PointCompare(uint16_t a = 0, uint16_t b = 0){
 int Tournament::addPoints(std::vector<game *> Games){
     size_t gamecount = Games.size();
     
-    for (game *i: Games)
+    for (game *i: Games)//* checking every game in the gamelist
     {
         int roundcount = i->getrounds();
-        for (int j = 0; j < roundcount; j++)
+        for (int j = 0; j < roundcount; j++)//* looping every round
         {
             
-            for (PlayerWithPoints k: i->players)
+            for (PlayerWithPoints k: i->players)//* do it for every player in playerlist
             {
-                k.AddPoint(1);
+                k.AddPoint(1);//* adding points (replace with actual input function)
             }
         }
     }
